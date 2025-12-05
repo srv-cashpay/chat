43da9beefce2c5fa89aa2f5a0520f1d31372b855
Asep rayana <asep.rayana09@gmail.com>
2025-12-05 15:06:37 +0700

    chat widget auth

--- repositories/widget/create.go
+++ repositories/widget/create.go
@@ -1,18 +1,31 @@
 package widget
 
 import (
+	"time"
+
 	"github.com/srv-cashpay/chat/dto"
+	"github.com/srv-cashpay/chat/entity"
 )
 
 func (r *widgetRepository) Create(req dto.WidgetRequest) (dto.WidgetResponse, error) {
-	if err := r.DB.Create(&req).Error; err != nil {
-		return dto.WidgetResponse{}, err
-	}
-	return dto.WidgetResponse{
+	chat := entity.ChatWidget{
 		ID:           req.ID,
 		FullName:     req.FullName,
 		Email:        req.Email,
 		Whatsapp:     req.Whatsapp,
 		BusinessName: req.BusinessName,
+		CreatedAt:    time.Now(),
+	}
+
+	if err := r.DB.Create(&chat).Error; err != nil {
+		return dto.WidgetResponse{}, err
+	}
+
+	return dto.WidgetResponse{
+		ID:           chat.ID,
+		FullName:     chat.FullName,
+		Email:        chat.Email,
+		Whatsapp:     chat.Whatsapp,
+		BusinessName: chat.BusinessName,
 	}, nil
 }
--- services/widget/create.go
+++ services/widget/create.go
@@ -8,26 +8,10 @@ import (
 )
 
 func (s *widgetService) Create(req dto.WidgetRequest) (dto.WidgetResponse, error) {
-	chat := dto.WidgetRequest{
-		ID:           util.GenerateRandomString(),
-		FullName:     req.FullName,
-		Email:        req.Email,
-		Whatsapp:     req.Whatsapp,
-		BusinessName: req.BusinessName,
-	}
-
-	// Simpan ke database
-	created, err := s.Repo.Create(chat)
+	req.ID = util.GenerateRandomString()
+	created, err := s.Repo.Create(req)
 	if err != nil {
-		return dto.WidgetResponse{}, fmt.Errorf("gagal menyimpan transaksi: %w", err)
+		return dto.WidgetResponse{}, fmt.Errorf("gagal menyimpan %w", err)
 	}
-
-	// Return response yang lebih lengkap
-	return dto.WidgetResponse{
-		ID:           created.ID,
-		FullName:     created.FullName,
-		Email:        created.Email,
-		Whatsapp:     created.Whatsapp,
-		BusinessName: created.BusinessName,
-	}, nil
+	return created, nil
 }
